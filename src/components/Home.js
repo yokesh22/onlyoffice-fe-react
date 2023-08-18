6e50ef26108855b33eee0f037c052266ccfc1a3c
yokesh22 <yokesh.ssmy2210@gmail.com>
2023-08-18 17:13:48 +0530

    iframe embed done

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+         <iframe
+        src=" https://vidhai.office.usln.in/vidhaioffice/exercise1/question1.html "
+        width="100%"
+        style={{ height: "100vh" }}
+      />
+    </div>
+  )
+}
+
+export default Home
