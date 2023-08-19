59025db9a3077ffc923d096d23e45ba55152ac16
yokesh22 <yokesh.ssmy2210@gmail.com>
2023-08-19 18:33:12 +0530

    .

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { DocumentEditor } from '@onlyoffice/document-editor-react';
+// import jwt from 'jsonwebtoken';
+
+var onDocumentReady = function (event) {
+    console.log("Document is loaded");
+};
+
+
+
+// const payload = {
+//     "sub": "1234567890",
+//     "name": "John Doe",
+//     "admin": true
+// };
+
+// const secretkey = "EIHqWso7Ld0AyZXXDQgqNDBWILWJOAmQ";
 
 function Home() {
-  return (
-    <div>
-         <iframe
-        src=" https://vidhai.office.usln.in/vidhaioffice/exercise1/question1.html "
-        width="100%"
-        style={{ height: "100vh" }}
-      />
-    </div>
-  )
+    // const token = jwt.sign(payload, secretkey);
+    
+    return (
+        <DocumentEditor
+            id="docxEditor"
+            documentServerUrl="https://editor.office.usln.in"
+            config={{
+                "document": {
+                    "fileType": "docx",
+                    'key': 'Khirz6zTPdfe',
+                    "title": "question1_solution.docx",
+                    "url": "https://editor.office.usln.in/example/editor?fileName=question1_solution.docx"
+                },
+                "token":"EIHqWso7Ld0AyZXXDQgqNDBWILWJOAmQ",
+                "documentType": "word"
+
+              
+            }}
+            events_onDocumentReady={onDocumentReady}
+        />
+    );
 }
 
-export default Home
+export default Home;
